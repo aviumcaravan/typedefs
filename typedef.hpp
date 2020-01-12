202130a804b33ed4095c7dde205f2d9b58a11e35
Wiktor Nowicki <59111798+wiktornowicki@users.noreply.github.com>
2020-01-12 11:40:53 +0100

    Add files via upload

---
+++ typedef.hpp
@@ -0,0 +1,32 @@
+#ifndef H_TYPEDEF
+#define H_TYPEDEF //definition macro
+
+typedef short shortInt;
+
+typedef int Int32;
+
+typedef long extInt;
+
+typedef char ASCII;
+
+typedef float Decimal;
+
+typedef double extDecimal;
+
+typedef const short cShortInt;
+
+typedef const int cInt32;
+
+typedef const long cExtInt;
+
+typedef const char cASCII;
+
+typedef const float cDecimal;
+
+typedef const double cExtDecimal;
+
+typedef bool flag;
+
+typedef const bool cFlag;
+
+#endif
